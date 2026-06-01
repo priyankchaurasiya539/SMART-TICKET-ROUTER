9906bc0a1175afafcdce8ca11ff5ff8f8785312f
Priyank Chaurasiya <priyankchaurasiya539@gmail.com>
2026-06-01 22:18:20 +0530

    Add Streamlit frontend UI connecting to prediction backend.

---
+++ frontend/app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import requests
+
+st.title("Smart Ticket Router UI")
+user_input = st.text_area("Enter your support description here ")
+
+if st.button("Route Ticket"):
+    url = "http://127.0.0.1:8000/predict"
+    data = {
+        "ticket_text" : user_input
+    }
+    response = requests.post(url , json=data)
+    result = response.json()
+    dept = result["department"]
+    priority = result["priority"]
+    st.success(f"Rooted to department : {dept}")
+    st.success(f"Assigned Priority : {priority}")
